3215a7d38e577c3a5e7b2f3fb8926be5c9442bb1
anuradha <anuradhasanjaya2024@gmail.com>
2024-11-19 22:56:27 +0530

    divided topbar into sub-components

---
+++ src/components/TopBar/NavSelector.jsx
@@ -0,0 +1,67 @@
+import Select, { components } from "react-select";
+import { SlArrowDown } from "react-icons/sl";
+import styles from "./Topbar.module.css";
+
+const options = [
+    { value: "home", label: "Home" },
+    { value: "contact", label: "Contact" },
+    { value: "about", label: "About" },
+];
+
+const customStyles = {
+    control: (provided, state) => ({
+        ...provided,
+        backgroundColor: "transparent",
+        border: "none",
+        boxShadow: state.isFocused ? "none" : "none",
+        color: "white",
+        fontSize: "1rem",
+        fontWeight: "200",
+        cursor: "pointer",
+    }),
+    singleValue: (provided) => ({
+        ...provided,
+        color: "white",
+    }),
+    menu: (provided) => ({
+        ...provided,
+        backgroundColor: "black",
+        width: "auto",
+        minWidth: "100%",
+    }),
+    option: (provided, state) => ({
+        ...provided,
+        backgroundColor: state.isSelected ? "gray" : "black",
+        color: "white",
+        fontSize: "1rem",
+        fontWeight: "200",
+        cursor: "pointer",
+        "&:hover": {
+            backgroundColor: "gray",
+        },
+    }),
+};
+
+const DropdownIndicator = (props) => {
+    return (
+        <components.DropdownIndicator {...props}>
+            <SlArrowDown color="white" />
+        </components.DropdownIndicator>
+    );
+};
+
+const NavSelector = () => {
+    return (
+        <Select
+            styles={customStyles}
+            components={{ DropdownIndicator }}
+            // className={styles.navSelect}
+            options={options}
+            placeholder="Select an option"
+            isClearable={false}
+            defaultValue={options[0]}
+        />
+    )
+};
+
+export default NavSelector;
--- src/components/TopBar/Topbar.jsx
+++ src/components/TopBar/Topbar.jsx
@@ -2,69 +2,8 @@ import { useState } from "react";
 import styles from "./Topbar.module.css";
 import { FaRegBell } from "react-icons/fa";
 import { FiMessageCircle } from "react-icons/fi";
-import Select, { components } from "react-select";
-import { SlArrowDown } from "react-icons/sl";
 import LoginModal from "../LoginModal/LoginModal";
-
-const options = [
-  { value: "home", label: "Home" },
-  { value: "contact", label: "Contact" },
-  { value: "about", label: "About" },
-];
-
-const customStyles = {
-  control: (provided, state) => ({
-    ...provided,
-    backgroundColor: "transparent",
-    border: "none",
-    boxShadow: state.isFocused ? "none" : "none",
-    color: "white",
-    fontSize: "1rem",
-    fontWeight: "200",
-    cursor: "pointer",
-  }),
-  singleValue: (provided) => ({
-    ...provided,
-    color: "white",
-  }),
-  menu: (provided) => ({
-    ...provided,
-    backgroundColor: "black",
-    width: "auto",
-    minWidth: "100%",
-  }),
-  option: (provided, state) => ({
-    ...provided,
-    backgroundColor: state.isSelected ? "gray" : "black",
-    color: "white",
-    fontSize: "1rem",
-    fontWeight: "200",
-    cursor: "pointer",
-    "&:hover": {
-      backgroundColor: "gray",
-    },
-  }),
-};
-
-const DropdownIndicator = (props) => {
-  return (
-    <components.DropdownIndicator {...props}>
-      <SlArrowDown color="white" />
-    </components.DropdownIndicator>
-  );
-};
-
-const NavSelector = () => (
-  <Select
-    styles={customStyles}
-    components={{ DropdownIndicator }}
-    className={styles.navSelect}
-    options={options}
-    placeholder="Select an option"
-    isClearable={false}
-    defaultValue={options[0]}
-  />
-);
+import NavSelector from "./NavSelector";
 
 const Topbar = () => {
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
