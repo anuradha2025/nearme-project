0f17050bf5fb7a0bb111d49616854b39b5ac32f9
Anuradha2k21 <61109105+Anuradha2k21@users.noreply.github.com>
2024-11-22 05:07:31 +0000

    update base path in Vite configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: "/",
+    base: "/nearme-project/",
   };
   if (command !== "serve") {
     config.base = "/nearme-project/";
